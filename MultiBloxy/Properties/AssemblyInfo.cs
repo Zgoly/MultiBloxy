541548bd641556f13564b2b3d703160dfe27fa32
Xelvanta <angelinawudirect@gmail.com>
2024-11-16 05:43:43 -0800

    Remove files not intended for pull request

--- MultiBloxy/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("MultiBloxy")]
-[assembly: AssemblyProduct("MultiBloxy")]
-[assembly: AssemblyCopyright("Copyright © Zgoly 2024")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
